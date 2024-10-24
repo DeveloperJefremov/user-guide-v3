298e7f78e2145df51b0b77841ef697d1ac6b67ab
Artjoms Jefremovs <developerjefremov@gmail.com>
2024-10-24 11:06:16 +0300

    bug fix : next config file improved

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'firebasestorage.googleapis.com',
+				port: '',
+				pathname: '/v0/b/**', // Указываем шаблон пути для корректной загрузки
+			},
+		],
+	},
+};
 
 export default nextConfig;
